9b7cd537550ff27a15ada4a9d78c9ef2493dd089
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-08-07 04:01:09 +0530

    built basic to-do list using javascript

---
+++ script.js
@@ -0,0 +1,39 @@
+const input = document.getElementById("input");
+const list = document.getElementById("list");
+
+function addTask(){
+    if(input.value===''){
+        alert('Please enter a task');
+    } else{
+        let li=document.createElement("li");
+        li.innerHTML = input.value;
+        list.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7"
+        li.appendChild(span);
+    } 
+    input.value='';
+    saveData();
+}
+list.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        const li=e.target;
+        li.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        alert("Do you want to delete this task?");
+        e.target.parentElement.remove();
+        saveData();
+    }
+    
+},false);                      
+
+function saveData(){
+    localStorage.setItem("data",list.innerHTML);
+}
+
+function showTask(){
+    list.innerHTML=localStorage.getItem("data");
+}
+showTask();
