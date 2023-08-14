a7c38a552a6a88bd667dfbb2da569b5688ac5404
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-08-15 02:03:40 +0530

    fixed background gradient issue

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 const input = document.getElementById("input");
 const list = document.getElementById("list");
 
-function addTask(){
-    if(input.value===''){
+function addTask() {
+    if (input.value === '') {
         alert('Please enter a task');
-    } else{
-        let li=document.createElement("li");
+    } else {
+        let li = document.createElement("li");
         li.innerHTML = input.value;
         list.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML="\u00d7"
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
         li.appendChild(span);
-    } 
-    input.value='';
+    }
+    input.value = '';
     saveData();
 }
-list.addEventListener("click",function(e){
-    if(e.target.tagName==="LI"){
-        const li=e.target;
+
+list.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        const li = e.target;
         li.classList.toggle("checked");
         saveData();
+    } else if (e.target.tagName === "SPAN") {
+        if (confirm("Do you want to delete this task?")) {
+            e.target.parentElement.remove();
+            saveData();
+        }
     }
-    else if(e.target.tagName==="SPAN"){
-        alert("Do you want to delete this task?");
-        e.target.parentElement.remove();
-        saveData();
-    }
-    
-},false);                      
 
-function saveData(){
-    localStorage.setItem("data",list.innerHTML);
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
 }
 
-function showTask(){
-    list.innerHTML=localStorage.getItem("data");
+function showTask() {
+    list.innerHTML = localStorage.getItem("data");
 }
 showTask();
+
+// New code to handle background gradient scrolling
+window.addEventListener('scroll', function () {
+    const scrollPercentage = (window.scrollY / (document.body.clientHeight - window.innerHeight)) * 100;
+    const gradientAngle = 150 + (scrollPercentage * 0.5);
+    const gradientColors = `linear-gradient(${gradientAngle}deg, black, rgb(22, 43, 205), rgb(190, 15, 15))`;
+
+    document.querySelector('.container').style.background = gradientColors;
+});
